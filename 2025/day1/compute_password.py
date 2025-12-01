4b17bab3a7795bbc6fd6b7a568aff5de954beec3
Elijah Bevers <elijah.bevers@oit.edu>
2025-11-30 21:49:11 -0800

    solved part 1 of day 1 problem

---
+++ 2025/day1/compute_password.py
@@ -0,0 +1,18 @@
+with open("input.txt", "r") as file:
+    input_string = file.read()
+
+dial_position = 50
+passcode = 0
+for command in input_string.split():
+    ticks = int(command[1:])
+    if command[0] == "L":
+        ticks = -ticks
+
+    # alternative is an R which means a positive increment
+    dial_position += ticks
+    dial_position %= 100 # euclidean modulo to emulate wrapping behavior of dial
+
+    if dial_position == 0:
+        passcode += 1 # passcode is the number of times the dial hits 0
+
+print("Passcode:", passcode)
