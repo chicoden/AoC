23491e3fc011cbfe0ebb11862031bbdcaa7ca1c4
Elijah Bevers <elijah.bevers@oit.edu>
2025-12-01 00:56:18 -0800

    small formatting change in solution to first part, added comments to solution to second part explaining flipping behavior

--- 2025/day1/compute_password.py
+++ 2025/day1/compute_password.py
@@ -7,8 +7,8 @@ for command in input_string.split():
     ticks = int(command[1:])
     if command[0] == "L":
         ticks = -ticks
-
     # alternative is an R which means a positive increment
+
     dial_position += ticks
     dial_position %= 100 # euclidean modulo to emulate wrapping behavior of dial
 
--- 2025/day1/compute_password2.py
+++ 2025/day1/compute_password2.py
@@ -11,12 +11,14 @@ for command in input_string.split():
     flipped = command[0] == "L"
 
     if flipped:
+        # flip so that a left rotation becomes a right rotation
         dial_position = -dial_position % 100
 
     passes_over_zero, dial_position = divmod(dial_position + ticks, 100)
     passcode += passes_over_zero
 
     if flipped:
+        # flip again to restore original orientation
         dial_position = -dial_position % 100
 
 print("Passcode:", passcode)
