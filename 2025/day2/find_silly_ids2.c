b1a5b859fff832dc4e599a9f5dd0275bce098f0e
Elijah Bevers <elijah.bevers@oit.edu>
2025-12-03 20:38:57 -0800

    almost have working solution to add up IDs with repeating subsequences of digits

--- 2025/day2/find_silly_ids2.c
+++ 2025/day2/find_silly_ids2.c
@@ -29,8 +29,47 @@ int read_u64(FILE* stream, uint64_t* value) {
     }
 }
 
+int count_digits(uint64_t value) {
+    int count = 0;
+    do {
+        count++;
+        value /= 10;
+    } while (value != 0);
+    return count;
+}
+
+int has_repeating_subsequence(uint64_t id) {
+    int id_length = count_digits(id);
+    int max_subseq_len = id_length / 2;
+
+    uint64_t subseq_mask = 10;
+    for (int subseq_len = 1; subseq_len <= max_subseq_len; subseq_len++) {
+        uint64_t subseq = id % subseq_mask;
+        //
+
+        uint64_t remaining_seq = id / subseq_mask;
+        int is_repeating = 1;
+        while (remaining_seq != 0) {
+            if (remaining_seq % subseq_mask != subseq) {
+                is_repeating = 0;
+                break;
+            }
+
+            remaining_seq /= subseq_mask;
+        }
+
+        if (is_repeating) {
+            return 1;
+        }
+
+        subseq_mask *= 10;
+    }
+
+    return 0;
+}
+
 int main() {
-    FILE* file = fopen("input2.txt", "rb");
+    FILE* file = fopen("input.txt", "rb");
     if (file == NULL) {
         puts("failed to open input file");
         return -1;
@@ -56,7 +95,12 @@ int main() {
             break;
         }
 
-        //
+        for (uint64_t id = start; id <= end; id++) {
+            if (has_repeating_subsequence(id)) {
+                printf("%llu\n", id);///
+                total += id;
+            }
+        }
     }
 
     fputs("Sum of invalid IDs: ", stdout);
