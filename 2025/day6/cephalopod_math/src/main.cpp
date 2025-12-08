fc43623ed7d019ae6d30b9d8fe54414b827dcae4
Elijah Bevers <elijah.bevers@oit.edu>
2025-12-08 06:00:32 -0800

    added querying and requesting of BDA and int64 math, added VMA to the project, and tentative shader code written

--- 2025/day6/cephalopod_math/src/main.cpp
+++ 2025/day6/cephalopod_math/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <array>
 #include <vector>
 #include <tuple>
 #include <string>
@@ -10,24 +11,19 @@
 #include <vulkan/vulkan.h>
 #include "housekeeper.hpp"
 
+#define VMA_IMPLEMENTATION
+#define VMA_VULKAN_VERSION 1003000 // Vulkan 1.3
+#include "vk_mem_alloc.h"
+
 struct QueueFamilyIndices {
     std::optional<uint32_t> compute;
-};
-
-struct WorkData {
-    VkDeviceMemory memory_backing;
-    VkBuffer add_problems;
-    VkBuffer mul_problems;
-    VkBuffer summation_scratch;
-    uint32_t add_problem_count;
-    uint32_t mul_problem_count;
-    uint32_t total_problem_count;
 };
 
 bool create_vulkan_instance(VkInstance& instance);
 std::tuple<VkPhysicalDevice, QueueFamilyIndices> pick_physical_device(VkInstance instance);
 QueueFamilyIndices find_queue_family_indices(VkPhysicalDevice gpu);
 bool create_logical_device(VkPhysicalDevice gpu, const QueueFamilyIndices& queue_family_indices, VkDevice& device);
+bool create_vma_allocator(VkInstance instance, VkPhysicalDevice gpu, VkDevice device, VmaAllocator& allocator);
 bool load_shader_spv(VkDevice device, VkShaderModule& shader_module, const char* path);
 
 int main(int argc, char* argv[]) {
@@ -60,6 +56,15 @@ int main(int argc, char* argv[]) {
         std::cout << "destroyed logical device" << std::endl;
     });
 
+    VmaAllocator allocator;
+    if (!create_vma_allocator(instance, gpu, device, allocator)) {
+        std::cout << "failed to create vma allocator" << std::endl;
+        return 0;
+    } Housekeeper cleanup_allocator([allocator]() {
+        vmaDestroyAllocator(allocator);
+        std::cout << "destroyed vma allocator" << std::endl;
+    });
+
     VkShaderModule math_shader;
     if (!load_shader_spv(device, math_shader, "shaders/cephalopod_math.spv")) {
         std::cout << "failed to load shader binary" << std::endl;
@@ -69,7 +74,42 @@ int main(int argc, char* argv[]) {
         std::cout << "destroyed shader module" << std::endl;
     });
 
-    std::cout << math_shader << std::endl;///
+    std::ifstream input_file(argv[1]);
+    if (!input_file.is_open()) {
+        std::cout << "failed to open input file" << std::endl;
+        return 0;
+    }
+
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(input_file, line)) {
+        lines.push_back(std::move(line));
+    }
+
+    std::string& ops_line = lines.back();
+    std::string::iterator ops_end = std::remove(ops_line.begin(), ops_line.end(), ' ');
+    std::string_view ops = std::string_view(ops_line.begin(), ops_end);
+
+    uint32_t add_problem_count = 0;
+    uint32_t mul_problem_count = 0;
+    for (char op : ops) {
+        switch (op) {
+            case '+': {
+                add_problem_count++;
+                break;
+            }
+
+            case '*': {
+                mul_problem_count++;
+                break;
+            }
+        }
+    }
+
+    uint32_t total_problem_count = add_problem_count + mul_problem_count;
+    uint32_t values_per_problem = lines.size() - 1; // numbers for each problem are arranged vertically
+
+    // create buffers
 
     return 0;
 }
@@ -107,16 +147,27 @@ std::tuple<VkPhysicalDevice, QueueFamilyIndices> pick_physical_device(VkInstance
     for (const auto& gpu : gpus) {
         uint32_t score = 0;
 
-        VkPhysicalDeviceProperties device_properties;
-        vkGetPhysicalDeviceProperties(gpu, &device_properties);
+        VkPhysicalDeviceProperties gpu_properties;
+        vkGetPhysicalDeviceProperties(gpu, &gpu_properties);
+
+        VkPhysicalDeviceFeatures2 gpu_features;
+        gpu_features.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2;
+        VkPhysicalDeviceBufferDeviceAddressFeatures gpu_bda_features;
+        gpu_bda_features.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_BUFFER_DEVICE_ADDRESS_FEATURES;
+        gpu_features.pNext = &gpu_bda_features;
+        gpu_bda_features.pNext = NULL;
+        vkGetPhysicalDeviceFeatures2(gpu, &gpu_features);
+
         QueueFamilyIndices queue_family_indices = find_queue_family_indices(gpu);
 
         if ( // can only receive a nonzero score if it is even suitable
-            queue_family_indices.compute.has_value()
+            queue_family_indices.compute.has_value() &&
+            gpu_features.features.shaderInt64 == VK_TRUE &&
+            gpu_bda_features.bufferDeviceAddress == VK_TRUE
         ) {
             score += 1; // suitable
 
-            switch (device_properties.deviceType) {
+            switch (gpu_properties.deviceType) {
                 case VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU: {
                     score += 1000;
                     break;
@@ -172,17 +223,33 @@ bool create_logical_device(VkPhysicalDevice gpu, const QueueFamilyIndices& queue
         create_info.pQueuePriorities = queue_priorities;
     }
 
-    VkPhysicalDeviceFeatures enabled_features{};
+    VkPhysicalDeviceFeatures2 enabled_features{};
+    enabled_features.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2;
+    enabled_features.features.shaderInt64 = VK_TRUE;
+    VkPhysicalDeviceBufferDeviceAddressFeatures enabled_bda_features{};
+    enabled_bda_features.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_BUFFER_DEVICE_ADDRESS_FEATURES;
+    enabled_bda_features.bufferDeviceAddress = VK_TRUE;
+    enabled_features.pNext = &enabled_bda_features;
+    enabled_bda_features.pNext = NULL;
 
     VkDeviceCreateInfo create_info{};
     create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
     create_info.queueCreateInfoCount = queue_create_infos.size();
     create_info.pQueueCreateInfos = queue_create_infos.data();
-    create_info.pEnabledFeatures = &enabled_features;
     create_info.enabledExtensionCount = 0;
     create_info.ppEnabledExtensionNames = NULL;
+    create_info.pNext = &enabled_features;
 
     return vkCreateDevice(gpu, &create_info, NULL, &device) == VK_SUCCESS;
+}
+
+bool create_vma_allocator(VkInstance instance, VkPhysicalDevice gpu, VkDevice device, VmaAllocator& allocator) {
+    VmaAllocatorCreateInfo create_info{};
+    create_info.vulkanApiVersion = VK_API_VERSION_1_3;
+    create_info.instance = instance;
+    create_info.physicalDevice = gpu;
+    create_info.device = device;
+    return vmaCreateAllocator(&create_info, &allocator) == VK_SUCCESS;
 }
 
 bool load_shader_spv(VkDevice device, VkShaderModule& shader_module, const char* path) {
@@ -202,19 +269,3 @@ bool load_shader_spv(VkDevice device, VkShaderModule& shader_module, const char*
 
     return vkCreateShaderModule(device, &create_info, NULL, &shader_module) == VK_SUCCESS;
 }
-
-/*bool load_math_problems(VkDevice device, WorkData& work_data, const char* path) {
-    std::ifstream file(path);
-    if (!file.is_open()) return false;
-
-    std::vector<std::string> lines;
-    std::string line;
-    while (std::getline(file, line)) {
-        lines.push_back(std::move(line));
-    }
-
-    std::string& ops_line = lines.back();
-    const std::string::iterator ops_end = std::remove(ops_line.begin(), ops_line.end(), ' ');
-    const std::string_view ops = std::string_view(ops_line.begin(), ops_end);
-    std::cout << '"' << ops << '"' << std::endl;
-}*/
