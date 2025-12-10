1cf1d085141193f7387454f30b3bec3affa71601
Elijah Bevers <elijah.bevers@oit.edu>
2025-12-09 22:33:40 -0800

    rearranged cephalopod math project to have CMakeLists.txt in the root and updating build scripts accordingly

--- 2025/day6/cephalopod_math/build_commands.bat
+++ 2025/day6/cephalopod_math/build_commands.bat
@@ -1,4 +1,4 @@
-cmake -D ERRORS_AS_WARNINGS=%1 -S src -B build -G "NMake Makefiles"
+cmake -D ERRORS_AS_WARNINGS=%1 -B build -G "NMake Makefiles"
 cd build
 nmake | findstr /v "vk_mem_alloc.h"
 .\CephalopodMath.exe "..\..\input.txt"
