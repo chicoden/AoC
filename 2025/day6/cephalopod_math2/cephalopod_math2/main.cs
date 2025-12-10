2d3d111a77e303e14868ce10c5e7e413cd505b88
Elijah Bevers <elijah.bevers@oit.edu>
2025-12-10 00:37:36 -0800

    moved code that doesn't depend on the StreamReader out of the using block

--- 2025/day6/cephalopod_math2/cephalopod_math2/main.cs
+++ 2025/day6/cephalopod_math2/cephalopod_math2/main.cs
@@ -21,57 +21,57 @@ namespace cephalopod_math2
                 return;
             }
 
+            List<string> lines = new List<string>();
             using (StreamReader reader = File.OpenText(input_path))
             {
-                List<string> lines = new List<string>();
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
                     lines.Add(line);
                 }
+            }
 
-                int line_length = lines[0].Length;
-                int max_digits = lines.Count - 1;
-                string ops = lines[max_digits];
+            int line_length = lines[0].Length;
+            int max_digits = lines.Count - 1;
+            string ops = lines[max_digits];
 
-                UInt64 total = 0UL;
-                for (int column = 0; column < line_length; column++)
+            UInt64 total = 0UL;
+            for (int column = 0; column < line_length; column++)
+            {
+                char op = ops[column];
+                UInt64 result = op == '+' ? 0UL : 1UL;
+                while (column < line_length)
                 {
-                    char op = ops[column];
-                    UInt64 result = op == '+' ? 0UL : 1UL;
-                    while (column < line_length)
-                    {
-                        UInt32 value = 0;
-                        bool column_empty = true;
-                        for (int place = 0; place < max_digits; place++)
-                        { // digits of each number are arranged in columns
-                            char digit = lines[place][column];
-                            if (digit != ' ')
-                            {
-                                value = value * 10 + (UInt32)(digit - '0');
-                                column_empty = false;
-                            }
-                        }
-
-                        if (column_empty)
-                        { // problems are seperated by a full column of spaces
-                            break;
-                        }
-
-                        switch (op)
+                    UInt32 value = 0;
+                    bool column_empty = true;
+                    for (int place = 0; place < max_digits; place++)
+                    { // digits of each number are arranged in columns
+                        char digit = lines[place][column];
+                        if (digit != ' ')
                         {
-                            case '+': result += value; break;
-                            case '*': result *= value; break;
+                            value = value * 10 + (UInt32)(digit - '0');
+                            column_empty = false;
                         }
-
-                        column++;
                     }
 
-                    total += result;
+                    if (column_empty)
+                    { // problems are seperated by a full column of spaces
+                        break;
+                    }
+
+                    switch (op)
+                    {
+                        case '+': result += value; break;
+                        case '*': result *= value; break;
+                    }
+
+                    column++;
                 }
 
-                Console.WriteLine($"Result: {total}");
+                total += result;
             }
+
+            Console.WriteLine($"Result: {total}");
         }
     }
 }
